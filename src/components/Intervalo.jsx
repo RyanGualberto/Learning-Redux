d4f20585baff73eac9430d77c2f28a08692023e8
Ryan <ryanoliveiragualberto@gmail.com>
2022-07-25 09:35:43 -0300

    s9a166: connect with redux #02

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,9 +1,9 @@
 import './Intervalo.css'
 import React from 'react'
-
 import Card from './Card'
+import { connect } from 'react-redux'
 
-export default props => {
+const Intervalo = props => {
     const {min, max} = props
 
     return (
@@ -11,13 +11,22 @@ export default props => {
             <div className="Intervalo">
                 <span>
                     <strong>Minimo</strong>
-                    <input type="number" value={0} readOnly />
+                    <input type="number" value={min} readOnly />
                 </span>
                 <span>
                     <strong>Máximo</strong>
-                    <input type="number" value={10} readOnly/>
+                    <input type="number" value={max} readOnly/>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state){
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Intervalo)
